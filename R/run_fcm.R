1679829055c88a384cd8b9de88d5bb84f0e4adfb
Julia Wrobel <julia.wrobel@cuanschutz.edu>
2023-05-17 10:00:06 -0600

    minimal working example for functional cox models

--- R/run_fcm.R
+++ R/run_fcm.R
@@ -1,12 +1,12 @@
 #' process_fcm
 #'
-#' Internal function called by \code{TITLE: regression function} that transforms long format functional data for use in a linear or additive functional Cox model.
+#' Fit a functional Cox regression model.
 #'
 #' @importFrom tidyr pivot_wider
 #' @importFrom mgcv gam
 #' @import dplyr
 #'
-#' @return A \code{dataframe} with matrix-valued covariates \code{l_int}, \code{t_int}, and \code{func} for use in a linear or additive functional Cox model.
+#' @return A \code{list} which is a linear or additive functional Cox model fit. See \code{mgcv::gam} for more details.
 #'
 #' @examples
 #' # simulate data
@@ -14,20 +14,23 @@
 #'
 #'
 #' @param mxfundata Dataframe of spatial summary functions from multiplex imaging data, in long format. Can be estimated using the function \code{extract_summary_functions} or provided separately.
+#' @param form Formula to be fed to mgcv in the form of survival_time ~ x1 + x2. Does not contain functional predictor. Character valued. Data must contain censoring variable called "event".
 #' @param id Character string, the name of the variable that identifies each unique subject.
 #' @param r Character string, the name of the variable that identifies the function domain (usually a radius for spatial summary functions). Default is "r".
 #' @param value Character string, the name of the variable that identifies the spatial summary function values. Default is "fundiff".
 #' @param knots Number of knots for defining spline basis.
-#' @param analysis_vars Optional list of variables to be retained for downstream analysis.
+#' @param afcm If TRUE, runs additive functional Cox model. If FALSE, runs linear functional cox model. Defaults to linear functional cox model.
+#' @param analysis_vars List of variables to be retained for downstream analysis, including variables from formula.
 #' @param quantile_transform Defaults to FALSE. If TRUE, a quantile transformation is applied to the functional predictor before modeling using the \code{process_fcm} function.
 #' @param ... Optional other arguments to be passed to \code{fpca.face}
 #' @export
 run_fcm <- function(mxfundata,
                         form,
-                        survival_status,
+                        event,
                         id,
                         r = "r",
                         value = "fundiff",
+                    afcm = FALSE,
                         analysis_vars,
                         quantile_transform = FALSE,
                         ...){
@@ -41,25 +44,25 @@ run_fcm <- function(mxfundata,
 
   mxfundata <- process_fcm(mxfundata, id, r, value, analysis_vars, quantile_transform)
 
-  # transform to matrix
-  # fit specified model
+  # fit linear or additive functional Cox model
+  if(afcm){
+    form =  paste0(form, '+ ti(t_int, func, by=l_int, bs=c("cr","cr"), k=c(10,10), mc=c(FALSE,TRUE))')
 
-  # TO DO: try providing formula in the form of a Surv object, so that you don't
-  # have so many arguments about specifying variables. BUT for now do what is fastest.
+    fit_fcm <- gam(formula = as.formula(form),
+                   weights = event,
+                   data = mxfundata, family = cox.ph())
 
-  fit_afc <- gam(survival_days ~ stage + adjuvant_therapy +
-                   ti(t_int, G, by=l_int, bs=c("cr","cr"), k=c(10,10), mc=c(FALSE,TRUE)),
-                 weights=survival_status,
-                 data=Gdf, family=cox.ph())
-
-
-  if(quantile_transform){
-    # quantile transformed version of the data
-    mxfundata$func <- apply(func, 2, function(y) ecdf(y)(y))
+    class(fit_fcm) <- "afcm"
   }else{
-    mxfundata$func <- func
+    form =  paste0(form, '+ s(t_int, by=l_int*func, bs="cr", k=10)')
+
+    fit_fcm <- gam(formula = as.formula(form),
+                   weights = event,
+                   data = mxfundata, family = cox.ph())
+
+    class(fit_fcm) <- "lfcm"
   }
 
-
+ fit_fcm
 }
 
