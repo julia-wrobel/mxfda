58ad412ec6c1b7c143401785b5afab4052bc3f8f
Julia Wrobel <julia.wrobel@cuanschutz.edu>
2023-05-30 10:38:21 -0600

    new files

---
+++ R/extract_surface.R
@@ -0,0 +1,80 @@
+#' extract_surface
+#'
+#' Internal function called by \code{TITLE: regression function} that transforms long format functional data for use in a linear or additive functional Cox model.
+#'
+#' @importFrom reshape2 melt
+#' @import dplyr
+#'
+#' @return A \code{dataframe} with predicted surface for AFCM and LFCM fits for use in plotting
+#'
+#' @examples
+#' # simulate data
+#' set.seed(1001)
+#'
+#'
+#' @param mxfundata Dataframe of spatial summary functions from multiplex imaging data, in long format.
+#' @param fit AFCM or LFCM model fit object
+#' @param r Character string, the name of the variable that identifies the function domain (usually a radius for spatial summary functions). Default is "r".
+#' @param value Character string, the name of the variable that identifies the spatial summary function values. Default is "fundiff".
+#' @param grid_length Length of grid on which to evaluate coefficient functions.
+#' @param analysis_vars Other variables used in modeling FCM fit.
+#' @param se Defaults to FALSE, and returns surface. If TRUE, returns coefficient function with standard errors for LFCM, or coefficient surface for AFCM with non NA values that are statistically significant
+extract_surface = function(mxfundata,
+                           fit,
+                       r = "r",
+                       value = "fundiff",
+                       grid_length = 100,
+                       analysis_vars,
+                       se = FALSE){
+
+  range_s = range(mxfundata[[r]], na.rm = TRUE)
+  range_x = range(mxfundata[[value]], na.rm = TRUE)
+  tind_pred <- seq(range_s[1], range_s[2], length.out = grid_length)
+  xind_pred <- seq(range_x[1], range_x[2], length.out = grid_length)
+
+  grid_coef <- data.frame(t_int = rep(tind_pred, grid_length),
+                          func = rep(xind_pred, each = grid_length), l_int=1)
+  grid_coef <- cbind(grid_coef, mxfundata[1, analysis_vars])
+
+  pred <- predict(fit, newdata = grid_coef, type = 'terms')
+  if(inherits(fit,"afcm")){
+    pred <- matrix(pred[,"ti(t_int,func):l_int"], ncol = sqrt(nrow(grid_coef)), nrow = sqrt(nrow(grid_coef)), byrow = FALSE)
+  }else{
+    pred <- matrix(pred[,"s(t_int):l_int * func"], ncol = sqrt(nrow(grid_coef)), nrow = sqrt(nrow(grid_coef)), byrow = FALSE)
+  }
+  if(se){
+    if(inherits(fit,"afcm")){
+      pred <- predict(fit, newdata = grid_coef, type = 'terms', se.fit = TRUE)
+
+      pred <- data.frame(beta1 = pred$fit[,"ti(t_int,func):l_int"],
+                            beta1_se = pred$se.fit[,"ti(t_int,func):l_int"])%>%
+        mutate(lower = beta1 - 1.96*beta1_se,
+               upper = beta1 + 1.96*beta1_se,
+               beta1_sig = ifelse(sign(lower) == sign(upper), beta1, NA))
+
+      pred <- matrix(pred$beta1_sig, ncol = sqrt(nrow(grid_coef)), nrow = sqrt(nrow(grid_coef)), byrow = FALSE)
+
+    }else{
+      grid_coef <- data.frame(t_int = tind_pred,
+                              func = 1,
+                              l_int=1)
+      grid_coef <- cbind(grid_coef, mxfundata[1, analysis_vars])
+
+      pred <- predict(fit, newdata = grid_coef, type = 'terms', se.fit = TRUE)
+
+      pred_df <- data.frame(r = tind_pred,
+                            beta1 = pred$fit[,"s(t_int):l_int * func"],
+                            beta1_se = pred$se.fit[,"s(t_int):l_int * func"])
+      return(pred_df)
+
+    }
+  }
+
+  rownames(pred) <- tind_pred
+  colnames(pred) <- xind_pred
+  melt(pred) %>%
+    rename(r = Var1, sumfun = Var2)
+}
+
+
+
---
+++ R/run_fcm.R
@@ -0,0 +1,74 @@
+#' process_fcm
+#'
+#' Fit a functional Cox regression model.
+#'
+#' @importFrom tidyr pivot_wider
+#' @importFrom mgcv gam
+#' @import dplyr
+#'
+#' @return A \code{list} which is a linear or additive functional Cox model fit. See \code{mgcv::gam} for more details.
+#'
+#' @examples
+#' # simulate data
+#' set.seed(1001)
+#'
+#'
+#' @param mxfundata Dataframe of spatial summary functions from multiplex imaging data, in long format. Can be estimated using the function \code{extract_summary_functions} or provided separately.
+#' @param form Formula to be fed to mgcv in the form of survival_time ~ x1 + x2. Does not contain functional predictor. Character valued. Data must contain censoring variable called "event".
+#' @param id Character string, the name of the variable that identifies each unique subject.
+#' @param r Character string, the name of the variable that identifies the function domain (usually a radius for spatial summary functions). Default is "r".
+#' @param value Character string, the name of the variable that identifies the spatial summary function values. Default is "fundiff".
+#' @param knots Number of knots for defining spline basis.
+#' @param afcm If TRUE, runs additive functional Cox model. If FALSE, runs linear functional cox model. Defaults to linear functional cox model.
+#' @param analysis_vars List of variables to be retained for downstream analysis, including variables from formula.
+#' @param quantile_transform Defaults to FALSE. If TRUE, a quantile transformation is applied to the functional predictor before modeling using the \code{process_fcm} function.
+#' @param smooth Option to smooth data using FPCA. Defaults to FALSE.
+#' @param ... Optional other arguments to be passed to \code{fpca.face}
+#' @export
+run_fcm <- function(mxfundata,
+                        form,
+                        id,
+                        r = "r",
+                        value = "fundiff",
+                    afcm = FALSE,
+                        analysis_vars,
+                        quantile_transform = FALSE,
+                    smooth = FALSE,
+                        ...){
+
+  # check for missing values in the functional predictor
+  if(smooth){
+    mxfundata <- impute_fpca(mxfundata, id = id, r = r, value = value,
+                             analysis_vars = analysis_vars, smooth = TRUE)
+    message("Functional predictor contains NA values that were imputed using FPCA")
+  }
+  if(anyNA(mxfundata[[value]])){
+    mxfundata <- impute_fpca(mxfundata, id = id, r = r, value = value,
+                               analysis_vars = analysis_vars)
+    message("Functional predictor contains NA values that were imputed using FPCA")
+  }
+
+  mxfundata <- process_fcm(mxfundata, id, r, value, analysis_vars, quantile_transform)
+
+  # fit linear or additive functional Cox model
+  if(afcm){
+    form =  paste0(form, '+ ti(t_int, func, by=l_int, bs=c("cr","cr"), k=c(10,10), mc=c(FALSE,TRUE))')
+
+    fit_fcm <- gam(formula = as.formula(form),
+                   weights = event,
+                   data = mxfundata, family = cox.ph())
+
+    class(fit_fcm) <- append("afcm", class(fit_fcm))
+  }else{
+    form =  paste0(form, '+ s(t_int, by=l_int*func, bs="cr", k=20)')
+
+    fit_fcm <- gam(formula = as.formula(form),
+                   weights = event,
+                   data = mxfundata, family = cox.ph())
+
+    class(fit_fcm) <- append("lfcm", class(fit_fcm))
+  }
+
+ fit_fcm
+}
+
