59c70231c75648b2dd1aa1e4eaa93c67993cb2a5
julia-wrobel <julia.wrobel@cuanschutz.edu>
2023-11-14 09:06:02 -0500

    added mfpca function

---
+++ R/run_mfpca.R
@@ -0,0 +1,105 @@
+#' run_fpca
+#'
+#' This is a wrapper for the function \code{mfpca.face} from the \code{refund} package. EXPAND
+#'
+#' @importFrom refund mfpca.face
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr pivot_wider
+#' @import dplyr
+#'
+#' @return A \code{mxFDA} object containing:
+#' \item{mxfundata}{The original dataframe of spatial summary functions, with scores from FPCA appended for downstream modeling}
+#' \item{fpc_object}{A list of class "fpca" with elements described in the documentation for  \code{refund::fpca.face}}
+#'
+#' @references Xiao, L., Ruppert, D., Zipunnikov, V., and Crainiceanu, C. (2016).
+#' Fast covariance estimation for high-dimensional functional data.
+#' \emph{Statistics and Computing}, 26, 409-421.
+#' DOI: 10.1007/s11222-014-9485-x.
+
+#' @examples
+#' # simulate data
+#' set.seed(1001)
+#'
+#'
+#' @param mxFDAobject object of class \code{mxFDA} created by `make_mxfda` with metrics derived with `extract_summary_functions`
+#' @param id Character string, the name of the variable that identifies each unique subject.
+#' @param image_id Character string, the name of the variable that identifies each unique image sample
+#' @param metric name of calculated spatial metric to use
+#' @param r Character string, the name of the variable that identifies the function domain (usually a radius for spatial summary functions). Default is "r".
+#' @param value Character string, the name of the variable that identifies the spatial summary function values. Default is "fundiff".
+#' @param knots Number of knots for defining spline basis.Defaults to the number of measurements per function divided by 2.
+#' @param analysis_vars Optional list of variables to be retained for downstream analysis.
+#' @param lightweight Default is FALSE. If TRUE, removes Y and Yhat from returned mFPCA object. A good option to select for large datasets.
+#' @param ... Optional other arguments to be passed to \code{mfpca.face}
+#' @export
+run_mfpca = function(mxFDAobject,
+                    id,
+                    metric = "uni k",
+                    r = "r",
+                    value = "fundiff",
+                    knots = NULL,
+                    analysis_vars = NULL,
+                    lightweight = FALSE,
+                    ...){
+  #get the right data
+  if(length(metric) != 1) stop("Please provide a single spatial metric to calculate functional PCA with")
+  metric = unlist(strsplit(metric, split = " "))
+
+  mxfundata = get_data(mxFDAobject, metric, 'summaries')
+
+  ## add stopper here to check if they provided a metric that doesn't exist
+  # right now we can split the patientImage_id but usually that won't work. Need to update S4 object to take both patient and image id
+  mxfundata = mxfundata %>%
+    separate(patientImage_id, into = c("id", "image_id"), sep = "_")
+
+
+  # NEED TO CHANGE THIS BEHAVIOR- WHAT DO WE DO IF WE HAVE AN NA FOR THE IMAGE SUMMARY FUNCTION
+  mxfundata = mxfundata %>% filter(!is.na(r))
+
+  index_range <- range(mxfundata[[r]])
+
+  # this seems to break when there are NA values, what behavior do I want for that?
+  mxfundata <- mxfundata %>%
+    select(all_of(c(id, image_id, r, value))) %>%
+    pivot_wider(names_from = r,
+                names_prefix = "r_",
+                values_from = value)
+
+  mxfundata %>%
+    arrange(id, image_id)
+
+  mat <- mxfundata %>%
+    select(starts_with("r_")) %>%
+    as.matrix()
+
+  if(is.null(knots)) knots = floor(ncol(mat)/3)
+  if(knots > ncol(mat) - 5) knots = floor(ncol(mat)/3)
+
+  # run fpca
+  mx_fpc <- fpca.face(Y = mat, knots = knots, ...)
+
+  if(lightweight){
+    mx_fpc$Y <- NULL
+    mx_fpc$Yhat <- NULL
+  }
+  mx_fpc$index_range <- index_range
+
+  score_df = setNames(as.data.frame(mx_fpc$scores), paste0("fpc", 1:mx_fpc$npc))
+
+  # append all FPCA scores to dataframe that has one row per subject, then convert to long format
+  mxfundata = bind_cols(mxfundata, score_df) %>%
+    select(-starts_with("r_"))
+
+  fpca_dat = list(score_df = score_df,
+       fpc_object = mx_fpc)
+
+  if(grepl("[B|b]", metric[1]) & grepl("[K|k]", metric[2])) mxFDAobject@`Functional PCA`$Kcross = fpca_dat
+  if(grepl("[B|b]", metric[1]) & grepl("[G|g]", metric[2])) mxFDAobject@`Functional PCA`$Gcross = fpca_dat
+  if(grepl("[B|b]", metric[1]) & grepl("[L|l]", metric[2])) mxFDAobject@`Functional PCA`$Lcross = fpca_dat
+  if(grepl("[U|u]", metric[1]) & grepl("[K|k]", metric[2])) mxFDAobject@`Functional PCA`$Kest = fpca_dat
+  if(grepl("[U|u]", metric[1]) & grepl("[G|g]", metric[2])) mxFDAobject@`Functional PCA`$Gest = fpca_dat
+  if(grepl("[U|u]", metric[1]) & grepl("[L|l]", metric[2])) mxFDAobject@`Functional PCA`$Lest = fpca_dat
+
+  return(mxFDAobject)
+
+}
