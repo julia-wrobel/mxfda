a974018bbfa565058bdda415cc322e82853b4dcb
ACSoupir <alex.soupir@jacks.sdstate.edu>
2023-11-17 12:29:09 -0500

    even better - no spatstat.explore attachment; export functions under mxfda namespace

---
+++ R/spatstat_funcs.R
@@ -0,0 +1,17 @@
+#' @export
+spatstat.explore::Kest
+
+#' @export
+spatstat.explore::Gest
+
+#' @export
+spatstat.explore::Lest
+
+#' @export
+spatstat.explore::Kcross
+
+#' @export
+spatstat.explore::Kcross
+
+#' @export
+spatstat.explore::Kcross
