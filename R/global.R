c7d154ca6d34672d1030fa45d06de425d88020a6
ACSoupir <alex.soupir@jacks.sdstate.edu>
2023-11-29 16:49:27 -0500

    build check issues, global variables with global.R

---
+++ R/global.R
@@ -0,0 +1,3 @@
+# utils::globalVariables(c('.', ':=', 'Var1', 'Var2', 'Yhat', beta1 beta1_se csr data ecdf fundiff
+#                          grid lower ovarian_FDA predict r setNames sumfuns theo upper value x
+#                          y))
