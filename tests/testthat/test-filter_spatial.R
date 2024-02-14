8f432a2cf1ee873caf21762e524f634fa6965b42
ACSoupir <alex.soupir@jacks.sdstate.edu>
2024-02-14 16:04:20 -0500

    update tests for filter_spatial

--- tests/testthat/test-filter_spatial.R
+++ tests/testthat/test-filter_spatial.R
@@ -13,24 +13,24 @@ mxFDAobject = make_mxfda(metadata = clinical,
                          sample_key = sample_id_column)
 
 test_that("output data same dims", {
-  mxFDAobject = filter_spatial(mxFDAobject, immune == "other")
+  mxFDAobject = filter_spatial(mxFDAobject, immune == "other", based_on = "spatial")
   expect_equal(nrow(mxFDAobject@Spatial), nrow(spatial %>% dplyr::filter(immune == "other")))
 })
 
 test_that("correct class", {
-  mxFDAobject = filter_spatial(mxFDAobject, immune == "other")
+  mxFDAobject = filter_spatial(mxFDAobject, immune == "other", based_on = "spatial")
   expect_equal(class(mxFDAobject)[1], "mxFDA")
 })
 
 test_that("column not in spatial", {
-  expect_error(filter_spatial(mxFDAobject, `T cell` == 1))
+  expect_error(filter_spatial(mxFDAobject, `T cell` == 1, based_on = "spatial"))
 })
 
 test_that("missing level in column - no force", {
-  expect_error(filter_spatial(mxFDAobject, immune == "T cells"))
+  expect_error(filter_spatial(mxFDAobject, immune == "T cells", based_on = "spatial"))
 })
 
 test_that("missing level in column - force", {
-  mxFDAobject = filter_spatial(mxFDAobject, immune == "T cell", force = TRUE)
+  mxFDAobject = filter_spatial(mxFDAobject, immune == "T cell", based_on = "spatial", force = TRUE)
   expect_equal(nrow(mxFDAobject@Spatial), 0)
 })
