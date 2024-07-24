d82fb1c95024336863a38dd2dea8ae0bb52b0551
ACSoupir <alex.soupir@jacks.sdstate.edu>
2024-07-24 09:46:53 -0400

    up to date
    Merge branch 'julia-dev' into alex-dev
    
    # Conflicts:
    #	R/extract_fpca_object.R
    #	R/run_fpca.R
    #	vignettes/mx_fda.Rmd

--- R/spatstat_funcs.R
+++ R/spatstat_funcs.R
@@ -1,60 +1,66 @@
 #test files
 #' Kest
 #'
-#' See `spatstat.explore::Kest`
+#' See [spatstat.explore::Kest()]
 #'
 #' @name Kest
 #' @export
 #' @keywords internal
 #' @importFrom spatstat.explore Kest
+#' @return object of class "fv" with columns for radii, theoretical K, and columns for edge corrections.
 NULL
 
 #' Gest
 #'
-#' See `spatstat.explore::Gest`
+#' See [spatstat.explore::Gest()]
 #'
 #' @name Gest
 #' @export
 #' @keywords internal
 #' @importFrom spatstat.explore Gest
+#' @return object of class "fv" with columns for radii, theoretical G, and columns for edge corrections.
 NULL
 
 #' Lest
 #'
-#' See `spatstat.explore::Lest`
+#' See [spatstat.explore::Lest()]
 #'
 #' @name Lest
 #' @export
 #' @keywords internal
 #' @importFrom spatstat.explore Lest
+#' @return object of class "fv" with columns for radii, theoretical L, and columns for edge corrections.
 NULL
 
 #' Kcross
 #'
-#' See `spatstat.explore::Kcross`
+#' See [spatstat.explore::Kcross()]
 #'
 #' @name Kcross
 #' @export
 #' @keywords internal
 #' @importFrom spatstat.explore Kcross
+#' @return object of class "fv" with columns for radii, theoretical bivariate K, and columns for edge corrections.
 NULL
 
 #' Gcross
 #'
-#' See `spatstat.explore::Gcross`
+#' See [spatstat.explore::Gcross()]
 #'
 #' @name Gcross
 #' @export
 #' @keywords internal
 #' @importFrom spatstat.explore Gcross
+#' @return object of class "fv" with columns for radii, theoretical bivariate G, and columns for edge corrections.
 NULL
 
 #' Lcross
 #'
-#' See `spatstat.explore::Lcross`
+#' See [spatstat.explore::Lcross()]
 #'
 #' @name Lcross
 #' @export
 #' @keywords internal
 #' @importFrom spatstat.explore Lcross
+#' @return object of class "fv" with columns for radii, theoretical bivariate L, and columns for edge corrections.
 NULL
